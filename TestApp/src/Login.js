2a6523aeeecd0a01cbe8dbadeaa2c676680beaba
mukeshnitp4u <mukeshnitp4u@gmail.com>
2019-02-26 02:15:32 +0530

    first commit

---
+++ TestApp/src/Login.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react'
+import {View , StatusBar , Text , Dimensions , Image , TextInput , TouchableOpacity}  from 'react-native'
+import testAsyncStorage from './TestAsyncStorage'
+import  utils from './Utils'
+import {connect} from 'react-redux'
+import {setUserData} from './redux/action'
+import commonStyles from './common/CommonStyles'
+import color from './common/color'
+var scrwidth=Dimensions.get('window').width
+var scrheight=Dimensions.get('window').height
+
+class Login extends Component {
+
+constructor(props){
+    super(props)
+    this.state = {
+        email : "",
+        password : ""
+    }
+}
+
+checkUserCredentials=()=>{
+    // testAsyncStorage.clear()
+    var mThis = this
+    testAsyncStorage.getData(this.state.email).then((value)=>{
+        if(value == null){
+            alert("User doesn't exist.")
+        } else{
+            var userData = JSON.parse(value);
+            var pass = userData.password;
+            if(pass === this.state.password){
+                testAsyncStorage.saveData('login', 'done');
+                mThis.props.setUserData(this.state.email)
+                utils.finishActivity(mThis , 'VideoScreen',null)
+            }else{
+                alert("Credential Wrong")
+            }
+        }
+    })
+}
+
+// #a11c4a #c21c4a
+  render() {
+    return (
+      <View style={commonStyles.container}>
+            <StatusBar 
+                backgroundColor = {color.primaryColor}
+            />
+
+            <View>
+                <Image style = {commonStyles.primaryImageView} source={require('../res/Image/alert.png')}/>
+                <Text style = {{fontSize:40,color:'white',alignSelf:'center'}}> Personal Content</Text>
+                <TextInput style = {commonStyles.primaryTextInput}
+                   
+                   underlineColorAndroid='pink'
+                   fontSize={17}
+                   placeholder = 'email'
+                   placeholderTextColor = "#808080"
+                   autoCapitalize = "none"
+                   onChangeText = {(text)=> this.setState({email:text})}
+                   />
+                <TextInput style = {commonStyles.primaryTextInput}
+                   underlineColorAndroid='pink'
+                   fontSize={17}
+                   placeholder = 'password'
+                   placeholderTextColor = "#808080"
+                   autoCapitalize = "none"
+                   secureTextEntry = {true}
+                   onChangeText = {(text)=> this.setState({password:text})}
+                />
+                <TouchableOpacity style={commonStyles.primaryButton}onPress={this.checkUserCredentials} >
+                    <Text style={commonStyles.buttonText}>Login</Text>
+                </TouchableOpacity>            
+            </View>
+            <View style = {{height:scrheight*0.1 , width:'100%' , position:'absolute' , bottom:0 , flexDirection:'row', justifyContent:'space-between',padding:'5%'}}>
+                <Text style={{color:'white',fontSize:16}}> Forgot Password </Text>
+                <Text style={{color:'white',fontSize:18}}> | </Text>
+                <Text onPress = {()=>utils.finishActivity(this , 'signUp' , null)} style={{color:'white',fontSize:16     , textDecorationLine :'underline'}}> Create an Account </Text>
+            </View>
+      </View>
+    )
+  }
+}
+
+const mapStateToProps = state => ({})
+
+
+function mapDispatchToProps(dispatch){
+    return {
+        setUserData : (data) => dispatch(setUserData(data))
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Login)
---
+++ TestApp/src/SignUp.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react'
+import {View , StatusBar , Text , Dimensions , Image , TextInput , TouchableOpacity , Platform , Alert}   from 'react-native'
+import testAsyncStorage from './TestAsyncStorage'
+import utils from './Utils';
+import ImagePicker  from 'react-native-image-picker'
+import commonStyles from './common/CommonStyles'
+import color from './common/color'
+var scrwidth=Dimensions.get('window').width
+var scrheight=Dimensions.get('window').height
+var selectedImagePath = ""
+export default class SignUp extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+             name  :'',
+             email :'',
+             password:'',
+             imageSource : ""
+        }
+    }
+
+    registerUser=()=>{
+        userObj = {
+            "name" : this.state.name,
+            "email" : this.state.email,
+            "password" : this.state.password,
+            "image" : this.state.imageSource
+        }
+        testAsyncStorage.saveData(this.state.email,userObj);
+        this.props.navigation.navigate('Login');
+    }
+
+    openImagePicker=()=>{
+        
+        const options = {
+            quality: 1.0,
+            maxWidth: 500,
+            maxHeight: 500,
+            storageOptions: {
+              skipBackup: true
+            }
+        }
+
+        ImagePicker.showImagePicker(options , (response) => {
+             if(response.didCancel){
+                 console.log("User pressed the cancel button")
+             }else if(response.error){
+                 console.log("ImagePicker Error : " +response.error)   
+                 if(Platform.OS == 'ios'){
+                    Alert.alert(
+                        'Please allow access',
+                        'Please enable the camera access',
+                        [
+                            {text : 'OK' ,onPress:()=> console.log("OK Pressed")}
+                        ],
+                        {cancelable : false}
+                    )
+                 }
+
+             }else{
+                 source ={uri : response.uri}
+                 selectedImagePath = response.uri
+                 this.setState({imageSource : selectedImagePath})
+             }
+        })
+    }
+
+
+  render() {
+    return (
+        <View style={commonStyles.container}>
+             <StatusBar 
+                backgroundColor = {color.primaryColor}
+            />
+
+            <View>
+                <TouchableOpacity>
+                {this.state.imageSource == "" ? (
+                    <Image style = {commonStyles.primaryImageView} source={require('../res/Image/user.png')}/>
+                ) :
+                (   <Image style = {{alignSelf:'center',margin:'10%' , height:120,width:120 , borderRadius : 60}} source={{uri : selectedImagePath}}/>
+                )
+                }
+                </TouchableOpacity>
+                <TextInput style = {commonStyles.primaryTextInput}
+                   underlineColorAndroid='pink'
+                   fontSize={17}
+                   placeholder = 'Full Name'
+                   placeholderTextColor = "#808080"
+                   autoCapitalize = "none"
+                   onChangeText = {(text)=> this.setState({name:text})}
+                   />
+                   <TextInput style = {commonStyles.primaryTextInput}
+                   underlineColorAndroid='pink'
+                   fontSize={17}
+                   placeholder = 'Email'
+                   placeholderTextColor = "#808080"
+                   autoCapitalize = "none"
+                   onChangeText = {(text)=> this.setState({email:text})}
+                   />
+                   <TextInput style = {commonStyles.primaryTextInput}
+                   underlineColorAndroid='pink'
+                   fontSize={17}
+                   placeholder = 'Password'
+                   placeholderTextColor = "#808080"
+                   autoCapitalize = "none"
+                   secureTextEntry={true}
+                   onChangeText = {(text)=> this.setState({password:text})}
+                   />
+                   <TouchableOpacity style={commonStyles.primaryButton}onPress={this.registerUser} >
+                    <Text style={commonStyles.buttonText}>Register</Text>
+                   </TouchableOpacity>
+            </View>
+            <View style={{position:'absolute',bottom:0 ,height:scrheight*.08 , width:'100%' , backgroundColor :'rgba(52 , 52 , 52 , 0.2)' , justifyContent:'center'}}>
+                    <Text onPress={()=> utils.finishActivity(this , 'Login' , null) }style={{alignSelf:'center' , color:'white' ,fontSize:18}}> Cancel </Text>
+            </View>
+        </View>
+    )
+  }
+}
